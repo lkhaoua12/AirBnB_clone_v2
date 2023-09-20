36cae95dec8c04a64549813c80f70cde3d32f4d4
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-09-20 23:58:18 +0100

    fixed import issues

--- models/city.py
+++ models/city.py
@@ -9,5 +9,5 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
 
     __tablename__ = 'cities'
-    name = Column(String(128), nuablle=False)
+    name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
