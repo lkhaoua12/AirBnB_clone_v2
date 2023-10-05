a9eccbc668c6bcf671e9c29050a13de70d91d44d
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-05 02:48:18 +0100

    shelchek compliante

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env bash
-# setup nginx for webstatic deployment
-
-# Install nginx if not alredy installed.
-if ! command -v nginx &> /dev/null
-then
-	sudo apt update -y
-	sudo apt install -y nginx
-fi
-
-# Use alias inside your Nginx configuration
-# create directory data and it subdirectories
-sudo mkdir -p /data/web_static/shared/
+# Bash script that sets up your web servers for the deployment of web_static.
+sudo apt update -y
+sudo apt install -y nginx
 sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared
 
-# create an index.html to check on nginx 
 echo "<!DOCTYPE html>
 <html>
   <head>
@@ -23,24 +14,8 @@ echo "<!DOCTYPE html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-# create a symnolic link. If the symbolic link already exists, it should be deleted and recreated every time the script is ran. 
-rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-
-#Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist). 
-#This should be recursive; everything inside should be created/owned by this user/group
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 
-# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static 
-
-config_file="/etc/nginx/sites-available/default"
-if ! grep -q "location /hbnb_static" $config_file
-then
-	sudo sed -i '/server {/a \
-    location /hbnb_static {\
-        alias /data/web_static/current/;\
-    }' $config_file
-fi
-
-# restart Nginx to apply the changes
+sudo sed -i '38 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
