628063be21a3ae5bc3108abd38f026d6477d791a
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-20 00:14:12 +0100

    render /

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" start a Flask app that renders Hello, World!"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_flask():
+    """ render hello hbnb whene called """
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
