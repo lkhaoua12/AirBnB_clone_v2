4be4c94fe428ace3a47e0e960b3172030f444155
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-20 00:16:10 +0100

    render / wiht strict_slashes to false

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def hello_flask():
     """ render hello hbnb whene called """
     return "Hello HBNB!"
