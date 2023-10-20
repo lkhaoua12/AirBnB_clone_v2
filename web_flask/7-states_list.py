0029c6f48e59886636fa3ab800295558f48fcba7
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-20 21:21:38 +0100

    fixed reload

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,15 +6,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
+
+
 @app.route('/states_list', strict_slashes=False)
 def list_states():
     states = storage.all("State").values()
     return render_template('7-states_list.html', states=states)
-
-
-@app.teardown_appcontext
-def teardown(exception):
-    storage.close()
 
 
 if __name__ == '__main__':
