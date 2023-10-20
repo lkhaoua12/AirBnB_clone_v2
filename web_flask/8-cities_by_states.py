70be5530c2c6bab9e7e5c61151ad49d854a44cac
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-21 00:14:51 +0100

    fixed order

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-""" render templates with states and cities """
+""" starts a Flask web application """
+
 from flask import Flask, render_template
-from models import storage, state
-
+from models.state import State
+from models import storage
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def close_app(exception):
-    storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    states = storage.all(state.State).values()
-    states = sorted(states, key=lambda state: state.name)
+    """display the states and cities."""
+    states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
 
 
-if __name__ == "__main__":
-    storage.reload()
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
