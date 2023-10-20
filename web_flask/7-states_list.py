c8bb3aacd5660bb340a2ea54acb9067171e62f04
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-20 21:29:20 +0100

    sorted list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,6 +14,7 @@ def teardown(exception):
 @app.route('/states_list', strict_slashes=False)
 def list_states():
     states = storage.all("State").values()
+    states = sorted(states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
 
 
