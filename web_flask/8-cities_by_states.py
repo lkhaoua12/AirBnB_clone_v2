ead03d00a3090ec2ebd5a10abe2ef3295663dad7
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-20 22:59:20 +0100

    cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" render templates with states and cities """
+from flask import Flask, render_template
+from models import storage, state
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_app(exception):
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    states = storage.all(state.State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
